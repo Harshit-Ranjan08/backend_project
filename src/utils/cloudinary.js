6a29e9b45d412d5c765bdf4d2a20a804c9e48857
Harshit Ranjan <harshitsah019@gmail.com>
2025-10-06 01:56:56 +0530

    api_form

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from "fs"
+
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary=async (localFilePath)=>{
+    try{
+        if(!localFilePath)return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath ,{
+            resource_type: "auto"
+        })
+
+        //sucessfully uploaded
+        console.log("file sucessfully uploaded",response.url);
+        return response;
+    }
+    catch(err)
+    {
+        fs.unlinkSync(localFilePath) 
+        //remove the locally saved path on server as the upload option failed
+        
+    }
+    
+}
+
+
+export {uploadOnCloudinary}
